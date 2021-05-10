de39a1bf98e890760e2ba9106de8cc4f4ef68464
Nitin Shrivastav <38551867+nitinshrivastav@users.noreply.github.com>
2021-05-10 18:23:45 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import pickle
+import pandas as pd
+
+
+# In[2]:
+
+
+file=open('apunkamodel.pkl','rb')
+model=pickle.load(file)
+
+
+# In[9]:
+
+
+from flask import Flask,render_template,request
+app=Flask(__name__)
+@app.route('/')
+def fkjfkjf():
+    return render_template('apnatimeaayega.html')
+@app.route('/info',methods=['GET','POST'])
+def fkjfk():
+    if(request.method=='POST'):
+        research=int(request.form['r'])
+        admin=int(request.form['a'])
+        market=int(request.form['m'])
+        res=model.predict([[research,admin,market]])
+        return render_template('apnatimeaayega.html',ans=res)
+if(__name__=='__main__'):
+    app.run()
+
+
+# In[6]:
+
+
+pwd
+
+
+# In[7]:
+
+
+file=open('adishivam.csv','w')
+file.close()
+
+
+# In[ ]:
+
+
+
+
